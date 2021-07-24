c8c587f8daa6df1fb78633f0258aef87d22d0d4a
Swann Martin <swannmartin1988@gmail.com>
2021-07-24 09:01:07 +0200

    remove input city and changed layout in mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './App.scss';
 import Meteo from './Meteo';
 
-
-
-
 function App() {
-
   const getCities = () => {
     const stringCities = localStorage.getItem('cities');
     if (!stringCities) return [];
     const data = JSON.parse(stringCities);
     return data;
-  }
+  };
 
-  const [inputCity, setInputCity] = useState('');
   const [inputZip, setInputZip] = useState('');
-  const [cities, setCities] = useState(getCities())
+  const [cities, setCities] = useState(getCities());
 
-  const createObject = (inputCity, inputZip) => {
+  const createObject = (inputZip) => {
     return {
-      city: inputCity,
       zip: inputZip,
-    }
-  }
+    };
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Quelle est la météo chez vous ?</h1>
       </header>
-      <main className='main'>
-        <form className="add-city-form" onSubmit={(e) => {
-          e.preventDefault()
-          const newObject = createObject(inputCity, inputZip);
-          setCities([newObject, ...cities])
-          setInputCity('');
-          setInputZip('');
-        }}>
-
+      <main className="main">
+        <form
+          className="add-city-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            const newObject = createObject(inputZip);
+            setCities([newObject, ...cities]);
+            setInputZip('');
+          }}
+        >
           <input
-            className='add-city-form-inputs'
-            type="text"
-            name="city"
-            placeholder="Ville"
-            value={inputCity}
-            onChange={(evt) => {
-              const text = evt.target.value;
-              setInputCity(text);
-
-            }} />
-          <input
-            className='add-city-form-inputs'
+            className="add-city-form-inputs"
             type="text"
             name="zip"
             placeholder="Code Postal"
@@ -59,19 +43,17 @@ function App() {
             onChange={(evt) => {
               const text = evt.target.value;
               setInputZip(text);
-
-            }} />
-          <button className='add-city-form-button'>Envoyer</button>
+            }}
+          />
+          <button className="add-city-form-button">Envoyer</button>
         </form>
-        <div className='cities'>
-          {
-            cities.map((element) => (
-              <Meteo key={element.city} city={element.city} zip={element.zip} />
-            ))
-          }
+        <div className="cities">
+          {cities.map((element) => (
+            <Meteo key={element.city} city={element.city} zip={element.zip} />
+          ))}
         </div>
       </main>
-    </div >
+    </div>
   );
 }
 
--- src/components/Meteo/index.js
+++ src/components/Meteo/index.js
@@ -2,50 +2,66 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './style.scss';
 
-const Meteo = ({ city, zip }) => {
+const Meteo = ({ zip }) => {
+  const [temp, setTemp] = useState(0);
+  const [sky, setSky] = useState('Cloud');
+  const [city, setCity] = useState('');
+  const localURL = 'http://localhost:1234/';
+  const liveURL = `https://api.openweathermap.org`;
+  const path = `/data/2.5/weather?zip=${zip},fr&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+  console.log(process.env.REACT_APP_API_KEY);
+  let url = liveURL + path;
+  // console.log(process.env.NODE_ENV)
+  // if (process.env.NODE_ENV === 'production') {
+  //     url = liveURL + path;
+  // }
 
-    const [temp, setTemp] = useState(0);
-    const [sky, setSky] = useState('Cloud');
-    const localURL = 'http://localhost:1234/';
-    const liveURL = `https://api.openweathermap.org`
-    const path = `/data/2.5/weather?zip=${zip},fr&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+  const fetchWeather = () => {
+    axios({
+      method: 'get',
+      url,
+    })
+      .then((res) => {
+        console.log('success voici les données', res.data);
+        setTemp(Math.round(res.data.main.temp));
+        setSky(res.data.weather[0].main);
+        setCity(res.data.weather[0].name);
+        console.log('name', res.data.weather[0].base);
+        console.log(sky);
+      })
+      .catch((err) => {
+        console.log('Ooops ça ne fonctionne pas', err);
+      });
+  };
 
-    let url = liveURL + path;
-    // console.log(process.env.NODE_ENV)
-    // if (process.env.NODE_ENV === 'production') {
-    //     url = liveURL + path;
-    // }
+  useEffect(fetchWeather, []);
 
-
-    const fetchWeather = () => {
-        axios({
-            method: 'get',
-            url,
-        })
-            .then(res => {
-                console.log('success voici les données', res.data)
-                setTemp(Math.round(res.data.main.temp));
-                setSky(res.data.weather[0].main)
-                console.log(sky);
-            })
-            .catch(err => {
-                console.log('Ooops ça ne fonctionne pas', err)
-            })
-
-    }
-
-    useEffect(fetchWeather, [])
-
-    return (
-        <div className="meteo">
-            <div className="meteo_city">
-                <h2>{city}</h2>
-                <p className="meteo_zip">{zip}</p>
-            </div>
-            <h3 className='meteo_temp'><span className={temp > 15 ? 'meteo_temp meteo_temp--hot' : 'meteo_temp meteo_temp--cold'} > {temp} </span>°c</h3>
-            {sky === 'Clouds' ? <i className="fas fa-cloud"></i> : <i className="fas fa-sun"></i>}
-            {sky === 'Clear' ? <i className="fas fa-sun"></i> : ''}
-        </div>
-    )
-}
-export default Meteo
+  return (
+    <div className="meteo">
+      <div className="meteo_city">
+        <h2>{city}</h2>
+        <p className="meteo_zip">{zip}</p>
+      </div>
+      <h3 className="meteo_temp">
+        <span
+          className={
+            temp > 15
+              ? 'meteo_temp meteo_temp--hot'
+              : 'meteo_temp meteo_temp--cold'
+          }
+        >
+          {' '}
+          {temp}{' '}
+        </span>
+        °c
+      </h3>
+      {sky === 'Clouds' ? (
+        <i className="fas fa-cloud"></i>
+      ) : (
+        <i className="fas fa-sun"></i>
+      )}
+      {sky === 'Clear' ? <i className="fas fa-sun"></i> : ''}
+    </div>
+  );
+};
+export default Meteo;
